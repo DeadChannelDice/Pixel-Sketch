27d90252232f407953f90446750ec92e25f453de
DeadChannelDice <michael.charpin@gmail.com>
2023-06-26 19:41:58 -0400

    Got the grid to generate and change with the slider.

---
+++ main.js
@@ -0,0 +1,47 @@
+const penColor = document.querySelector("pen-color")
+const eraserBtn = document.querySelector("#btn-eraser")
+const rainbowMode = document.querySelector("#btn-rainbow")
+const normalMode = document.querySelector("#btn-normal")
+const clearArea = document.querySelector("#btn-clear")
+const shadowMode = document.querySelector("#btn-shadow")
+const highlightMode = document.querySelector("#btn-highlight")
+const gridSize = document.querySelector("#slider")
+const drawingArea = document.querySelector(".drawing-area")
+const numOfRows = document.querySelector(".num-of-rows")
+
+numOfRows.textContent = `${gridSize.value} x ${gridSize.value}`
+
+const genGrid = (numofColumns) => {
+
+    drawingArea.innerHTML = ""
+
+
+    drawingArea.style.gridTemplateColumns = `repeat(${numofColumns}, 1fr)`
+
+    let pixelCount = (numofColumns * numofColumns)
+
+    for(let i = 0; i < pixelCount; i++){
+        const pixel = document.createElement("div")
+        pixel.classList.add("pixel")
+        drawingArea.appendChild(pixel)
+    }
+
+}
+
+
+
+genGrid(gridSize.value)
+
+
+
+// Link slider to grid resolution 
+gridSize.addEventListener("change", e => {
+    genGrid(e.target.value)
+    numOfRows.textContent = `${gridSize.value} x ${gridSize.value}`
+    console.log(e.target.value)
+})
+
+
+// Make pixels change colors when clicking and dragging
+
+
